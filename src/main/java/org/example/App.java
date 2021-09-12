de8e923a411c0ff4aa2ba3e5d8cb9ba33acfd979
CailaC <caila.coates2010@gmail.com>
2021-09-12 16:38:52 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Caila Coates
+ */
+import java.util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.next();
+        System.out.println("Hello, " + name + ", nice to meet you!");
+
+
+    }
+}
